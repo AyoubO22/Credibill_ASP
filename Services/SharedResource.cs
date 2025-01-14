9a6def2bf23ce8a1cceaffeb864ba34a1116292d
Ayoub Ouaadoud <ayoub.ouaadoud@student.ehb.be>
2025-01-14 16:06:28 +0100

    ressource files

---
+++ Services/SharedResource.cs
@@ -0,0 +1,7 @@
+﻿namespace CrediBill_ASP
+{
+    // Dit is een lege class die aan de basis-namespace wordt toegevoegd om de shard resource localizer file te beheren
+    public class SharedResource
+    {
+    }
+}
